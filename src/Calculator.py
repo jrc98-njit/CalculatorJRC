a276568ffd7bc8ad66004ee883e3b58d8e5dbaf5
jrc98-njit <85322136+jrc98-njit@users.noreply.github.com>
2021-07-02 18:19:42 -0400

    Feature: Added CsvReader code.

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,6 +1,4 @@
-
 class Calculator:
-    result = 0
 
     def __int__(self):
         pass
--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,9 +1,23 @@
 import unittest
 from Calculator import Calculator
 
+
 class MyTestCase(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
+    def test_instantiate_calculator(self):
+        calculator = Calculator()
+        self.assertIsInstance(self.calculator, Calculator)
+
+    def test_add_method_calculator(self):
+        self.assertEqual(self.calculator.add)
+        self.assertEqual(self.calculator.result)
 
 
-    if __name__ == '__main__':
-        unittest.main()
+
+
+
+if __name__ == '__main__':
+    unittest.main()
