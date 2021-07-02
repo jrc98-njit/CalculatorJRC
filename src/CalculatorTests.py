03e762f0fd4a9177a3926172c5b61e4d609e4cbc
jrc98-njit <85322136+jrc98-njit@users.noreply.github.com>
2021-07-02 17:27:01 -0400

    Feature: Setup Calculator class and test

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,10 +1,9 @@
 import unittest
-
+from Calculator import Calculator
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
 
 
-if __name__ == '__main__':
-    unittest.main()
+
+    if __name__ == '__main__':
+        unittest.main()
