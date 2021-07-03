9b69ae6571bf50514d29a54e4ba853009ca49884
jrc98-njit <85322136+jrc98-njit@users.noreply.github.com>
2021-07-02 22:54:05 -0400

    Feature: Successful test for individual methods in calculator.

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -12,35 +12,30 @@ class MyTestCase(unittest.TestCase):
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.calculator, Calculator)
 
- #   def test_addition(self):
- #       test_Data = CsvReader('/src/UT_Addition.csv').data
- #       for row in test_Data:
- #           self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']),int(row['Result']))
+    def test_addition(self):
+        test_Data = CsvReader('/src/UT_Addition.csv').data
+        for row in test_Data:
+            self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']),int(row['Result']))
 
+    def test_subtraction(self):
+        test_Data = CsvReader('/src/UT_Subtraction.csv').data
+        for row in test_Data:
+            self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']),int(row['Result']))
 
-#    def test_subtraction(self):
-#        test_Data = CsvReader('/src/UT_Subtraction.csv').data
-#        for row in test_Data:
-#            self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']),int(row['Result']))
+    def test_multiply(self):
+        test_Data = CsvReader('/src/UT_Multiply.csv').data
+        for row in test_Data:
+            self.assertEqual(self.calculator.multiply(row['Value 1'], row['Value 2']), int(row['Result']))
 
-
-#   def test_multiply(self):
-#     test_Data = CsvReader('/src/UT_Multiply.csv').data
-#     for row in test_Data:
-#          self.assertEqual(self.calculator.multiply(row['Value 1'], row['Value 2']), int(row['Result']))
-
-
-#    def test_division(self):
-#        test_Data = CsvReader('/src/UT_Division.csv').data
-#        for row in test_Data:
-#            self.assertEqual(self.calculator.division(row['Value 1'], row['Value 2']), float(row['Result']))
-
+    def test_division(self):
+        test_Data = CsvReader('/src/UT_Division.csv').data
+        for row in test_Data:
+            self.assertEqual(self.calculator.division(row['Value 1'], row['Value 2']), float(row['Result']))
 
     def test_square(self):
         test_Data = CsvReader('/src/UT_Square.csv').data
         for row in test_Data:
             self.assertEqual(self.calculator.square(row['Value 1']), int(row['Result']))
-
 
     def test_square(self):
         test_Data = CsvReader('/src/UT_SquareRoot.csv').data
