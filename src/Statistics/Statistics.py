fd33bf3d69f51b6b09539e6dabf58a1df4295b85
jrc98-njit <85322136+jrc98-njit@users.noreply.github.com>
2021-07-12 10:35:53 -0400

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 5c520b2a4afaafeb02d6982e7bb2a1108a4f4b3a, reversing
    changes made to 4406a297335f62626a6d1af58020057970b71b55.

---
+++ src/Statistics/Statistics.py
@@ -0,0 +1,8 @@
+from Calculator.Calculator import Calculator
+
+
+class Statistics(Calculator):
+    data = []
+
+    def __int__(self):
+        super().__int__()
---
+++ src/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='Calculator',
+      verson='0.0.1',
+      description='Its a calculator',
+      author='',
+      url='',
+      packages=find_packages(),
+      )
